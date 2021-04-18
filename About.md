814dfc96b7e722b65938bea9d220c4c2d4a9ccc2
kn0rr <knorr_tobias@web.de>
2021-04-18 15:47:21 +0200

    Finished project

---
+++ About.md
@@ -0,0 +1,19 @@
+# About this Project
+
+This project is about React Class and about React State and also has an implementation of some custom css and some lifecycle method like `componentDidMount`.
+
+It shows some customized screen based on the Geolocation and the corresponding session.
+
+## Dependencies
+
+It uses `semantic-ui` for styling purpose
+
+````html
+ <!--index.html-->
+ <link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+/>
+````
+
+It also uses the [Geolocation API](https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API) to display the users positon.
--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* Old version with Function component
+
+const App=()=>{
+    window.navigator.geolocation.getCurrentPosition(
+        (position)=> console.log(position),
+        (err)=>console.log(err)
+    );
+
+return (
+    <div>Latitude: </div>
+)
+}; */
+
+// Must have: extend React component
+class App extends React.Component{
+
+    state={lat:null, errorMessage:''};
+
+    componentDidMount(){
+        console.log('My component was rendered to the screen');
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=>{
+                this.setState({lat:position.coords.latitude});
+            },
+            (err)=>{
+                this.setState({errorMessage:'Cannot show the Geolocation'})
+            }
+        );
+    }
+    componentDidUpdate(){
+        console.log('My component was updated');
+    }
+    
+    renderContent(){
+        if(this.state.errorMessage && !this.state.lat){
+            return  <div>
+            Error:{this.state.errorMessage}
+            </div>
+        }
+        if (!this.state.errorMessage && this.state.lat){
+            return  <SeasonDisplay lat={this.state.lat}/>
+        }
+   return <Spinner message="Please accept location request"/>
+    }
+
+    // Must Have: Define Render method to return jsx
+    render() {    
+        return( 
+      <div className="border red">
+          {this.renderContent()}
+      </div>
+      )
+    }
+}
+
+
+ReactDOM.render(<App/>,document.querySelector('#root'))
